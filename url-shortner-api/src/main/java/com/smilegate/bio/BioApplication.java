208cb1d16e7c4187a972d5d9d4f9b11752bd0322
kor-Chipmunk <rhj4862@gmail.com>
2023-12-07 20:54:24 +0900

    chore: 멀티 모듈 프로젝트로 변경

--- url-shortner-api/src/main/java/com/smilegate/bio/BioApplication.java
+++ url-shortner-api/src/main/java/com/smilegate/bio/BioApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BioApplication.class, args);
+    }
 
 }
