cfe493cac53ce22b9e6397ec318689d6428b6b58
kor-Chipmunk <rhj4862@gmail.com>
2023-12-10 10:58:57 +0900

    feat: URL 정상인지 검사하는 로직 추가

--- url-shortner-api/src/main/java/com/smilegate/bio/service/ShortURLService.java
+++ url-shortner-api/src/main/java/com/smilegate/bio/service/ShortURLService.java
@@ -4,6 +4,7 @@ import com.smilegate.bio.dto.CreateShortURLRequestDTO;
 import com.smilegate.bio.dto.ShortURLDTO;
 import com.smilegate.bio.entity.ShortURL;
 import com.smilegate.bio.repository.ShortURLRepository;
+import java.net.URL;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,10 +17,22 @@ public class ShortURLService {
 
     @Transactional
     public ShortURLDTO createShortURL(CreateShortURLRequestDTO request) {
+        final String originURL = request.originUrl();
+        validateOriginURL(originURL);
+
         final ShortURL shortURL = new ShortURL(request.originUrl());
 
         shortURLRepository.save(shortURL);
 
         return ShortURLDTO.from(shortURL);
+    }
+
+    private void validateOriginURL(String originURL) {
+        try {
+            URL url = new URL(originURL);
+            url.toURI();
+        } catch (Exception exception) {
+            throw new IllegalArgumentException();
+        }
     }
 }
