fa084dfd9c17a94ab0f9f0175ba0f2ef64a4f5f6
kor-Chipmunk <rhj4862@gmail.com>
2023-12-11 17:50:44 +0900

    feat: Base62 문자 목록 순서 알 수 없도록 랜덤으로 배열

--- url-shortner-api/src/main/java/com/smilegate/bio/util/Base62Util.java
+++ url-shortner-api/src/main/java/com/smilegate/bio/util/Base62Util.java
@@ -5,23 +5,15 @@ public class Base62Util {
     private static final int MAX_LENGTH = 8;
     private static final int RADIX = 62;
 
-    private static final char[] SCHEMES;
-
-    static {
-        SCHEMES = new char[RADIX];
-
-        for (int i = 0; i < 10; i++) {
-            SCHEMES[i] = (char) ('0' + i);
-        }
-
-        for (int i = 0; i < 26; i++) {
-            SCHEMES[10 + i] = (char) ('A' + i);
-        }
-
-        for (int i = 0; i < 26; i++) {
-            SCHEMES[36 + i] = (char) ('a' + i);
-        }
-    }
+    private static final char[] SCHEMES = new char[] {
+            'V', 'y', 'x', 'l', 'Q', 'L', 'N', 'S', 'O', '9',
+            'H', 'T', '8', 'J', 'u', 'i', 'X', 'D', 'K', 'c',
+            '7', 'h', 'F', 'a', '3', 'b', 'o', '5', 'g', 'M',
+            'f', 'E', 'W', 'Z', '2', 'R', 'p', '6', 'G', 'q',
+            'v', 'r', 'k', '1', 'U', 'n', 'P', 'w', 'e', 't',
+            '0', 'm', 'B', 'Y', 'C', '4', 'j', 'd', 'I', 'z',
+            'A', 's'
+    };
 
     private Base62Util() {
         // No Instances
--- url-shortner-api/src/test/java/com/smilegate/bio/util/Base62UtilTest.java
+++ url-shortner-api/src/test/java/com/smilegate/bio/util/Base62UtilTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class Base62UtilTest {
     @ParameterizedTest
-    @CsvSource(value = {"10:A" , "100000:u0Q", "100000000:EZal6"}, delimiter = ':')
+    @CsvSource(value = {"10:H" , "100000:jVo", "100000000:uRpwN"}, delimiter = ':')
     @DisplayName("62진법으로 고유 번호를 문자열로 변환합니다.")
     void Should_Base62_Encryption_When_Id_Serve(long id, String expected) {
         //given
