7c374225ee6d05bddfcaf5685294062b1e642159
kor-Chipmunk <rhj4862@gmail.com>
2023-12-13 07:30:48 +0900

    test: 기본 애플리케이션 테스트 추가

---
+++ ticket-api/src/test/java/com/smilegate/ticket/TicketApplicationTest.java
@@ -0,0 +1,13 @@
+package com.smilegate.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
