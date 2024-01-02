0d09fdf21026a5997810485e9840632dd1b28ec7
kor-Chipmunk <rhj4862@gmail.com>
2024-01-02 16:59:05 +0900

    feat: 주소를 찾지 못하는 경우 베이스 주소로 리다이렉트 & 로깅 추가

--- url-shortner-api/src/main/java/com/smilegate/urlshortner/controller/RedirectionController.java
+++ url-shortner-api/src/main/java/com/smilegate/urlshortner/controller/RedirectionController.java
@@ -3,6 +3,10 @@ package com.smilegate.urlshortner.controller;
 import com.smilegate.urlshortner.dto.ShortURLDTO;
 import com.smilegate.urlshortner.service.RedirectionService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,13 +17,23 @@ import org.springframework.web.servlet.view.RedirectView;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/")
+@Slf4j
 public class RedirectionController {
+    private final Logger logger = LoggerFactory.getLogger(RedirectionController.class);
+
     private final RedirectionService redirectionService;
+    private final Environment environment;
 
     @GetMapping("{id}")
     public RedirectView redirectOriginURL(@PathVariable("id") String shortUrl) {
-        final ShortURLDTO shortURLDTO = redirectionService.getShortURLByShortUrl(shortUrl);
-        final String originURL = shortURLDTO.originUrl();
+        String originURL = environment.getProperty("redirect.web.baseurl");
+
+        try {
+            final ShortURLDTO shortURLDTO = redirectionService.getShortURLByShortUrl(shortUrl);
+            originURL = shortURLDTO.originUrl();
+        } catch (RuntimeException exception) {
+            logger.warn(String.format("찾을 수 없는 단축 주소를 요청했습니다. - %s", shortUrl));
+        }
 
         final RedirectView redirectView = new RedirectView(originURL);
         redirectView.setStatusCode(HttpStatus.PERMANENT_REDIRECT);
